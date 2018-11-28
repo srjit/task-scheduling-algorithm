01643a8916788d8fbe2efae5ca44b62c6a626347
Sreejith Sreekumar <sreejith2904@gmail.com>
2018-11-27 20:30:25 -0500

    update data for code

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
+using namespace std;
+
+
+void print_matrix(int **matrix, int rows, int cols){
+
+  for(int i=0; i<rows; i++){
+    for(int j=0; j<cols; j++){
+      cout<< matrix[i][j] << "\t";
+    }
+    cout<<"\n";
+  }
+  
+}
 
 void example_graph(int **graph, int jobCount){
 
@@ -7,10 +20,11 @@ void example_graph(int **graph, int jobCount){
     graph[i] = new int[jobCount];
   }
 
-  int a[10] = {1,1,1,0,1,1,1,1,0,1};
-  graph[0] = a;
-  
-   
+  graph[0][1] = graph[0][2] = graph[0][3] = graph[0][4]
+              = graph[0][5] = graph[1][7] = graph[1][8]
+              = graph[2][6] = graph[3][7] = graph[3][8]
+              = graph[4][8] = graph[5][7] = graph[6][9]
+              = graph[7][9] = graph[8][9] = 1;
 }
 
 int main(){
@@ -22,6 +36,7 @@ int main(){
 
   example_graph(graph, jobCount);
 
-  std::cout<<graph[0][8];
+  print_matrix(graph, 10, 10);
+
   
 }
